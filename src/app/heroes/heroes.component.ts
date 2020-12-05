b6e8a37339abe8f74c1151c625094bf5c9f0717b
Konrad Korf <konrad.korf@gmail.com>
2020-12-05 13:43:35 +0100

    2/6

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,18 +8,14 @@ import { HEROES } from '../mock-hereos';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes = HEROES;
+  selectedHero: Hero
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  selectedHero: Hero
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
